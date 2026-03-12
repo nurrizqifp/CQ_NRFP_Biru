48e26e79214b448789da8c79fee38529dbde9e8b
nurrizqifp <nurrizqifebriansyah@gmail.com>
2026-03-12 16:47:20 +0700

    Add SqlConnection and SqlCommand to Form1 class
    Declared SqlConnection and SqlCommand fields in Form1 to enable database operations using System.Data.SqlClient.

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -13,6 +13,8 @@ namespace PraktikumADO
 {
     public partial class Form1 : Form
     {
+        SqlConnection conn;
+        SqlCommand cmd;
         public Form1()
         {
             InitializeComponent();
