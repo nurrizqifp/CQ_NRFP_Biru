14a58bad9ecd317739ce1a7e31f4841278de5344
nurrizqifp <nurrizqifebriansyah@gmail.com>
2026-03-12 19:51:38 +0700

    Add exception handling for Update SKS button

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -142,7 +142,19 @@ namespace PraktikumADO
 
         private void btnUpdateSKS_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Koneksi();
+                conn.Open();
 
+                string query = "UPDATE MataKuliah SET SKS = 4 WHERE KodeMK = 'IF210101'";
+                cmd = new SqlCommand(query, conn);
+
+                int hasil = cmd.ExecuteNonQuery();
+                MessageBox.Show("Berhasil update! Jumlah baris terpengaruh: " + hasil);
+
+                conn.Close();
+            }
         }
 
         private void btnInsertProdi_Click(object sender, EventArgs e)
