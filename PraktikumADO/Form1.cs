efa001c1745d9d05b9df9841e22fded22327fcad
nurrizqifp <nurrizqifebriansyah@gmail.com>
2026-03-12 19:53:52 +0700

    Add catch exception for Insert Prodi button on click

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -176,6 +176,10 @@ namespace PraktikumADO
 
                 conn.Close();
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
